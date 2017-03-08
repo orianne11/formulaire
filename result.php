3253ea780ae06ee4c84573fe1ced1b134848d56d
orianne11 <orianne.r11@gmail.com>
2017-03-08 09:52:33 +0100

    envoie formulaire

---
+++ result.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+
+<?php
+echo "Prénom tapé par l'utilisateur : ".$_POST['prenom'] . "<br>";
+echo "nom tapé par l'utilisateur : ".$_POST['nom'] . "<br>";
+echo "age tapé par l'utilisateur : ".$_POST['age'] . "<br>";
+echo "langage tapé par l'utilisateur : ".$_POST['langage'] . "<br>";
+
+?>
+</body>
+</html>
